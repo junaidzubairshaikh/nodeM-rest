817f35205d96b979b4e610e89b8a704dac78b989
junaidzubairshaikh <cse.zubair.shaikh@gmail.com>
2018-12-10 21:25:17 +0500

    worker file

--- config.js
+++ config.js
@@ -4,14 +4,24 @@ environments.staging={
     'httpPort':3000,
     'httpsPort':3001,
     'envName':'staging',
-    'hashingSecret':'thisissecret'
+    'hashingSecret':'thisissecret',
+    'twilio':{
+        'accoundSid':'AC558a86bd84df2b5670c48ee646ddd51f',
+        'authToken':'697e6ce81c39b3cd5174c05846dea01a',
+        'from':'+923423678748'
+    }
 }
 
 environments.production={
     'httpPort':5000,
     'httpsPort':5001,
     'envName':'production',
-    'hashingSecret':'thisissecret'
+    'hashingSecret':'thisissecret',
+    'twilio':{
+        'accoundSid':'AC558a86bd84df2b5670c48ee646ddd51f',
+        'authToken':'697e6ce81c39b3cd5174c05846dea01a',
+        'from':'+923423678748'
+    }
 }
 
 var currentEnvironment=typeof(process.env.NODE_ENV)=='string'?process.env.NODE_ENV.toLocaleLowerCase():'';
--- index.js
+++ index.js
@@ -1,92 +1,13 @@
-const http=require('http');
-const https=require('https');
-const url=require('url');
-const fs=require('fs');
-const StringDecoder=require('string_decoder').StringDecoder;
-const environment=require('./config');
-const libs=require('./lib/libs');
-const handlers=require('./lib/handlers');
-const helper=require('./lib/helpers');
+var server=require('./lib/server');
+var worker=require('./lib/worker');
 
+var app={};
 
-//Create a file
-// libs.create('test','newFile',{'hello':'world'},function(err){
-//         console.log(err);
-// });
-
-//read file
-
-// libs.read('test','newFile',(data)=>{
-//  console.log('Reading file...',data)
-// });
-
-// libs.update('test','newFile',{'key':'hello world'},(msg)=>{
-//     console.log(msg);
-// });
-
-// libs.delete('test','newFile',(msg)=>{
-//     console.log(msg);
-// })
-
-
-const httpsServerOption={
-    key:fs.readFileSync('./https/key.pem'),
-    cert:fs.readFileSync('./https/cert.pem')
+app.init=function(){
+    server.init();
+    // worker.init();
 }
 
-const httpServer=http.createServer(function(req,res){
-    unifiedServer(req,res);
-});
 
-httpServer.listen(environment.httpPort,(req,res)=>{
-    console.log('The server is listening on port ',environment.httpPort);
-});
+app.init();
 
-const httpsServer=https.createServer(httpsServerOption,function(req,res){
-    unifiedServer(req,res);
-});
-
-httpsServer.listen(environment.httpsPort,(req,res)=>{
-    console.log('The server is listening on port', environment.httpsPort)
-});
-
-var unifiedServer=function(req,res){
-    var parsedUrl=url.parse(req.url,true);
-  
-    var trimmedPath=parsedUrl.pathname.replace(/^\/+|\+$/g,'');
-
-    var decoder=new StringDecoder("utf-8");
-    var buffer='';
-    req.on('data',function(data){
-        buffer+=decoder.write(data);
-    });
-
-    req.on('end',function(){
-        buffer+=decoder.end();
-
-        var routerResolver=typeof(router[trimmedPath])!=='undefined'?router[trimmedPath]:handlers.notFoundHandler;
-
-        var data={
-            'trimmedPath':trimmedPath,
-            'query':parsedUrl.query,
-            'headers':req.headers,
-            'method':req.method.toLowerCase(),
-            'payload':helper.pasrseJson(buffer)
-        }
-        routerResolver(data,function(statusCode,payload){
-            statusCode=typeof(statusCode)=='number'?statusCode:200;
-            payload=typeof(payload)=='object'?payload:{};
-            res.setHeader('Content-Type','application\json');
-            res.writeHead(statusCode);
-            res.end(JSON.stringify(payload));
-                console.log('Resolver with status code '+statusCode,'with data ',payload);
-        });
-
-    });
-}
-
-const router={
-    hello:handlers.helloHandler,
-    users:handlers.users,
-    tokens:handlers.token
-}
